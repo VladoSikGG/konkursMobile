a23fd4993473a0a0b8eb78afcf868a79df6b9f25
RUDIK <vladislavrudakovskij@gmail.com>
2024-03-06 04:46:25 +0300

    FixedBugWithReg

--- app/src/main/java/com/example/konkurs/MainActivity.kt
+++ app/src/main/java/com/example/konkurs/MainActivity.kt
@@ -3,13 +3,14 @@ package com.example.konkurs
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+Log.d("TEST", FirebaseAuth.getInstance().currentUser.toString())
         var intent = Intent(applicationContext, LoginActivity::class.java)
         if(FirebaseAuth.getInstance().currentUser == null)
             startActivity(intent)
--- app/src/main/java/com/example/konkurs/RegisterActivity.kt
+++ app/src/main/java/com/example/konkurs/RegisterActivity.kt
@@ -3,6 +3,8 @@ package com.example.konkurs
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Debug
+import android.util.Log
 import android.widget.Toast
 import com.example.konkurs.databinding.ActivityLoginBinding
 import com.example.konkurs.databinding.ActivityRegisterBinding
@@ -13,36 +15,42 @@ import com.google.firebase.database.FirebaseDatabase
 
 class RegisterActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityRegisterBinding
+private lateinit var binding: ActivityRegisterBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        var intent = Intent(applicationContext, RegisterActivity::class.java)
-        var main = Intent(applicationContext, MainActivity::class.java)
-        binding = ActivityRegisterBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    var intent = Intent(applicationContext, RegisterActivity::class.java)
+    var main = Intent(applicationContext, MainActivity::class.java)
+    binding = ActivityRegisterBinding.inflate(layoutInflater)
+    setContentView(binding.root)
 
-        binding.btnRegister.setOnClickListener{
-            if(binding.regEmail.text.toString().isEmpty() || binding.regPass.text.toString().isEmpty() || binding.regNick.text.toString().isEmpty()){
-                Toast.makeText(applicationContext, "Fields is empty", Toast.LENGTH_SHORT).show()
-            }else{
-                FirebaseAuth.getInstance().createUserWithEmailAndPassword(binding.regEmail.text.toString(), binding.regPass.text.toString())
-                    .addOnCompleteListener(OnCompleteListener <AuthResult>{ task ->
-                        if(task.isSuccessful){
-                            var userInfo = HashMap<String, String>()
-                            userInfo.put("email", binding.regEmail.text.toString())
-                            userInfo.put("pass", binding.regPass.text.toString())
-                            userInfo.put("nick", binding.regNick.text.toString())
-                            FirebaseDatabase.getInstance().getReference().child("Users").setValue(userInfo)
-                            startActivity(main)
-                        }
+    binding.btnRegister.setOnClickListener{
+        if(binding.regEmail.text.toString().isEmpty() || binding.regPass.text.toString().isEmpty() || binding.regNick.text.toString().isEmpty()){
+            Toast.makeText(applicationContext, "Fields is empty", Toast.LENGTH_SHORT).show()
+        }else{
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(binding.regEmail.text.toString(), binding.regPass.text.toString())
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        var userInfo = HashMap<String, String>()
+                        Log.d("TEST", "createdMap")
+                        userInfo["email"] = binding.regEmail.text.toString()
+                        userInfo["pass"] = binding.regPass.text.toString()
+                        userInfo["nick"] = binding.regNick.text.toString()
+                        FirebaseDatabase.getInstance("https://konkurs-91da2-default-rtdb.europe-west1.firebasedatabase.app/")
+                            .reference.child("users")
+                            .child(FirebaseAuth.getInstance().currentUser!!.uid)
+                            .setValue(userInfo)
+                        startActivity(main)
+                    }
+                }
 
-                    })
-
-
-            }
         }
 
 
+
+
+        }
     }
+
+
 }
